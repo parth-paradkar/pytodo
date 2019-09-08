a36aab11e704045f7e2b75418a7e68c5c827f215
Parth Paradkar <parthparadkar3@gmail.com>
2019-09-08 15:13:52 +0530

    Whitespace removed

--- priority.py
+++ priority.py
@@ -4,6 +4,4 @@ def date_sort(todo_list):
     dated_todos = [element for element in todo_list if element['due'] != None]
     non_dated_todos = [element for element in todo_list if element['due'] == None]
     return sorted(dated_todos, key = lambda i: i['due']) + non_dated_todos
-
-
-    
+ 
