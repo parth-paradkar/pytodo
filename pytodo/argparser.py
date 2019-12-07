b6ef732ceddedee36faa539fdc580593ef393d52
Yashwant Singh <yashwantsinghtp@gmail.com>
2019-12-07 23:17:34 +0530

    added --expire argument
    when script is invoked with this argument all overdue todos whose due date is
    earlier that the date on which the script is called will be delted irrespective of
    the their status

--- pytodo/argparser.py
+++ pytodo/argparser.py
@@ -17,4 +17,5 @@ def create_parser():
     )
     parser.add_argument("--edit", action="append", help="edit an existing todo")
     parser.add_argument("--clear", action="append", help="delete all todos")
+    parser.add_argument("--expire", action="append", help="delete all overdue todos")
     return parser
