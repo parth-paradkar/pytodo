ebcc25dc1048425ce9c3bff3e040615e65bf3db4
Janish <JSjanish.1999@gmail.com>
2019-12-09 20:31:54 +0530

    Added import for disable_quotes in script.py

--- pytodo/script.py
+++ pytodo/script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from argparser import create_parser
-from modules import add_todo, get_all_todos, display_todos, set_todo_status, delete_by_index, clear, update_by_index, expire_todos, expire_overdue_todos
+from modules import add_todo, get_all_todos, display_todos, set_todo_status, delete_by_index, clear, update_by_index, expire_todos, expire_overdue_todos,disable_quotes
 
 
 def main():
